4e2965e2e4bf373a6604d74fdbc3f259a0fc56e9
mahmoud hamdy <mh1522489@gmail.com>
2026-07-16 15:59:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,14 @@
 // تحديث تلقائي عند تعديل الموقع
 // ============================================
 
-const CACHE_NAME = 'yallabuy-cache-v7';
+const CACHE_NAME = 'yallabuy-cache-v8';
 const CORE_ASSETS = [
   '/',
-  'index.html',
-  'style.css',
-  'script.js',
-  'logo.png',
-  'favicon.ico'
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/logo.png',
+  '/favicon.ico'
 ];
 
 // ============================================

b256e2b2cab9980ddafdf8b272ba4f42fa52248a
mahmoud hamdy <mh1522489@gmail.com>
2026-07-16 16:19:11 +0300

    Update cache name to v13

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // تحديث تلقائي عند تعديل الموقع
 // ============================================
 
-const CACHE_NAME = 'yallabuy-cache-v12';
+const CACHE_NAME = 'yallabuy-cache-v13';
 const CORE_ASSETS = [
   '/',
   '/index.html',

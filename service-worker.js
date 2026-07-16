683b04c1fb384fcc4dcb2148cf5db635dd19e3e2
mahmoud hamdy <mh1522489@gmail.com>
2026-07-17 01:13:19 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // كاشات منفصلة لكل نوع
 // ============================================
 
-const CACHE_NAME = 'yallabuy-pages-v6';
+const CACHE_NAME = 'yallabuy-pages-v7';
 const IMAGE_CACHE = 'yallabuy-images-v1';
 const STATIC_CACHE = 'yallabuy-static-v1';
 

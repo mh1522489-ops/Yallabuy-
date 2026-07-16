797bab96930f1473a7e776bc6de3bd66c4843ece
mahmoud hamdy <mh1522489@gmail.com>
2026-07-16 16:00:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -63,7 +63,7 @@ self.addEventListener('fetch', (event) => {
         return networkResponse;
       }).catch(() => {
         if (request.mode === 'navigate') {
-          return caches.match('/index.html');
+          return caches.match('/');
         }
       });
     })
